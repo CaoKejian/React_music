988c433e608e5e5d8fe32686eb9e3d3aac919f02
曹珂俭 <1849201815@qq.com>
2023-06-29 22:50:16 +0800

    完成点击导航切换和样式

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,11 @@
 import React, { memo } from 'react'
 import type { FC, ReactNode } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, NavLink, useLocation } from 'react-router-dom'
 import s from './Header.module.scss'
 import logo from '../assets/imgs/logo.png';
 import { SearchOutlined } from '@ant-design/icons';
 import { Input } from 'antd';
-
+import { CaretUpOutlined } from '@ant-design/icons';
 interface IProps {
   children?: ReactNode
 }
@@ -15,8 +15,18 @@ const Header: FC<IProps> = () => {
     "发现音乐": "/discover",
     "我的音乐": "/mine",
     "我的关注": "/focus",
+    "商城": "/1",
+    "音乐人": "/2",
     "下载客户端": "/download",
   }
+  const barClick = () => {
+    console.log(1);
+
+  }
+
+  const location = useLocation();
+  console.log(location.pathname);
+
   return (
     <div className={s.wrapper}>
       <div className={s.content}>
@@ -24,7 +34,18 @@ const Header: FC<IProps> = () => {
         <Link to="/" className={s.logoname}>Colin音乐</Link>
         {
           Object.entries(mapHub).map(([key, value]) => {
-            return <Link to={value}>{key}</Link>
+            return <><NavLink
+              onClick={() => barClick()}
+              className={({ isActive }) => {
+                return isActive ? `${s.active}` : undefined
+              }}
+              key={key} to={value}>
+              {key}
+              {location.pathname.includes(value) && (
+                <CaretUpOutlined className={s.icon} style={{ color: '#c20c0c' }} />
+              )}
+            </NavLink>
+            </>
           })
         }
         <div className={s.rightdiv}>
@@ -34,6 +55,7 @@ const Header: FC<IProps> = () => {
           <a href='/'>登录</a>
         </div>
       </div>
+      <div className={s.divider}>111</div>
     </div>
 
   )
