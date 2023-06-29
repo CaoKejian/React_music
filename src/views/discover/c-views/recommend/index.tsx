7e520d1325ef9a3a3dbe495d97f0cc5cd49a417e
曹珂俭 <1849201815@qq.com>
2023-06-29 16:35:45 +0800

    封装axios & 获取轮播图数据

--- src/views/discover/c-views/recommend/index.tsx
+++ src/views/discover/c-views/recommend/index.tsx
@@ -1,13 +1,42 @@
-import React, { memo } from 'react'
+import React, { memo, useEffect, useState } from 'react'
 import type { FC, ReactNode } from 'react'
+import hyRequest from '@/service'
 
 interface IProps {
   children?: ReactNode
 }
+ interface IBannerRoot {
+  imageUrl: string
+  targetId: number
+  adid: any
+  targetType: number
+  titleColor: string
+  typeTitle: string
+  url: any
+  exclusive: boolean
+  video: any
+  song: any
+  scm: string
+  bannerBizType: string
+}
 
 const Recommend: FC<IProps> = () => {
+  const [banners, setBanners] = useState<IBannerRoot[]>([])
+  useEffect(() => {
+    hyRequest.get({
+      url: "/banner"
+    }).then(res => {
+      setBanners(res.banners)
+    })
+  })
   return (
-    <div>Recommend</div>
+    <div>
+      {
+        banners.map((item,index) => {
+          return <div key={index}>{item.imageUrl}</div>
+        }) 
+      }
+    </div>
   )
 }
 
