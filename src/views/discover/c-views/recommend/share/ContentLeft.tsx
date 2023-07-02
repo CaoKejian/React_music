3b704228521436051239b0fa6a3a19b69737028d
曹珂俭 <1849201815@qq.com>
2023-07-02 22:24:37 +0800

    完成：轮播图功能

--- src/views/discover/c-views/recommend/share/ContentLeft.tsx
+++ src/views/discover/c-views/recommend/share/ContentLeft.tsx
@@ -1,5 +1,5 @@
-import React, { memo } from 'react'
-import type { FC, ReactNode } from 'react'
+import React, { memo,useRef } from 'react'
+import type { FC, ReactNode,ElementRef } from 'react'
 import s from './contentLeft.module.scss'
 import Title from '@/components/Title'
 import { useAppSelevtor } from '@/store'
@@ -9,6 +9,7 @@ import {
   CaretLeftOutlined,
   CaretRightOutlined,
 } from '@ant-design/icons';
+import { Carousel } from 'antd'
 
 interface IProps {
   children?: ReactNode
@@ -19,6 +20,13 @@ const ContentLeft: FC<IProps> = () => {
   const { hotRecommends } = useAppSelevtor((state) => ({
     hotRecommends: state.recommend.hotRecommend
   }), shallowEqual)
+  const bannerRef = useRef<ElementRef<typeof Carousel>>(null)
+  const handlePrev = () => {
+    bannerRef.current?.prev()
+  }
+  const handleNext = () => {
+    bannerRef.current?.next()
+  }
   return (
     <div className={s.wrapper}>
       <Title arr={TitleArr} title='热门推荐' gotoRoute='/discover/songs'></Title>
@@ -32,8 +40,22 @@ const ContentLeft: FC<IProps> = () => {
       <Title title='新碟上架' gotoRoute='/discover/album'></Title>
       <div className={s.newDisc}>
         <div className={s.autopic}>
-          <CaretLeftOutlined className={`${s.button} ${s.button1}`} style={{ fontSize: '18px', color: '#898989' }} />
-          <CaretRightOutlined className={`${s.button} ${s.button2}`} style={{ fontSize: '18px', color: '#898989' }} />
+          <div className={s.banner}>
+            <Carousel ref={bannerRef} className={s.autoPic} 
+              autoplaySpeed={3000}
+              speed={2000}
+              dots={false}
+              // beforeChange={handleBeforeChange}
+            >
+              {
+                [1,2,3].map((item, index) => {
+                  return <h1 key={index}>{item}</h1>
+                })
+              }
+            </Carousel>
+          </div>
+          <CaretLeftOutlined onClick={handlePrev} className={`${s.button} ${s.button1}`} style={{ fontSize: '18px', color: '#898989' }} />
+          <CaretRightOutlined onClick={handleNext}  className={`${s.button} ${s.button2}`} style={{ fontSize: '18px', color: '#898989' }} />
         </div>
       </div>
     </div>
