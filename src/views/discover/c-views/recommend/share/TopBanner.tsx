0de50e6afea6ab86f171dba3a2391d7ba15640d9
曹珂俭 <1849201815@qq.com>
2023-07-02 14:35:39 +0800

    完成轮播图自定义样式

--- src/views/discover/c-views/recommend/share/TopBanner.tsx
+++ src/views/discover/c-views/recommend/share/TopBanner.tsx
@@ -1,9 +1,8 @@
-import React, { memo, useEffect, useLayoutEffect, useRef, useState } from 'react'
+import { memo, useEffect, useRef, useState } from 'react'
 import type { FC, ReactNode, ElementRef } from 'react'
 import s from './TopBanner.module.scss'
 import { useAppSelevtor } from '@/store'
 import { Carousel } from 'antd';
-import { flushSync } from 'react-dom';
 
 import {
   RightOutlined,
@@ -24,24 +23,25 @@ const TopBanner: FC<IProps> = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
   const [bgImgUrl, setBgImgUrl] = useState('');
 
-  useLayoutEffect(() => {
+  useEffect(() => {
     const imgUrl = banners[currentIndex]?.imageUrl;
     if (imgUrl) {
       setBgImgUrl(imgUrl + '?imageView&blur=40x20');
     }
   }, [banners, currentIndex]);
 
-  const handleAfterChange = (current: number, next: number) => {
+  const handleBeforeChange = (current: number, next: number) => {
     setCurrentIndex(next);
   };
-
+ 
   return (
     <div className={s.wrapper} style={{ backgroundImage: `url('${bgImgUrl}')` }}>
       <div className={s.auto}>
         <Carousel ref={bannerRef} autoplay className={s.autoPic} effect="fade" easing="ease-out"
           autoplaySpeed={3000}
           dots={false}
-          beforeChange={handleAfterChange}>
+          beforeChange={handleBeforeChange}
+          >
           {
             banners.map((item, index) => {
               return <div className={s.imgItem} key={index}>
@@ -50,6 +50,16 @@ const TopBanner: FC<IProps> = () => {
             })
           }
         </Carousel>
+        <ul className={s.dots}>
+          {
+            banners.map((item, index) => {
+              return <li key={item.imageUrl}>
+                <span 
+                className={index === currentIndex ? s.active : s.item} ></span>
+              </li>
+            })
+          }
+        </ul>
         <LeftOutlined onClick={() => bannerRef.current?.prev()} className={`${s.button} ${s.button1}`} style={{ fontSize: '54px', color: '#fff' }} />
         <RightOutlined onClick={() => bannerRef.current?.next()} className={`${s.button} ${s.button2}`} style={{ fontSize: '54px', color: '#fff' }} />
         <div className={s.floatPic}></div>
